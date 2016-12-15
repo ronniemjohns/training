e90f3513d290944810b3bc7265c7f73ec6514542
Ronnie Johns <rjohns7@gatech.edu>
2016-12-14 21:44:44 -0600

    Ronnie - Original push of java brains hibernate project

---
+++ JavaBrainsHibernate/src/main/java/com/javabrains/hibernate/entity/UserDetails.java
@@ -0,0 +1,40 @@
+package com.javabrains.hibernate.entity;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class UserDetails {
+	
+	@Id
+	private int userId;
+	
+	private String userName;
+	
+	
+	public UserDetails() {
+		
+	}
+	
+	public UserDetails(int userId, String userName) {
+		super();
+		this.userId = userId;
+		this.userName = userName;
+	}
+
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+	public String getUserName() {
+		return userName;
+	}
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+	
+	
+
+}
