e4d55f50919e0233b65c11fb2d9dde0a88c3aa85
Ronnie Johns <rjohns7@gatech.edu>
2016-12-21 00:32:01 -0600

    Ronnie - tutorial portion covering various additional annotations (Basic, Lob, Temporal, etc...).

--- JavaBrainsHibernate/src/main/java/com/javabrains/hibernate/HibernateTest.java
+++ JavaBrainsHibernate/src/main/java/com/javabrains/hibernate/HibernateTest.java
@@ -1,5 +1,7 @@
 package com.javabrains.hibernate;
 
+import java.util.Date;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -13,6 +15,9 @@ public class HibernateTest {
 		
 		userDetails.setUserId(1);
 		userDetails.setUserName("User 1");
+		userDetails.setAddress("First user's address.");
+		userDetails.setJoinedDate(new Date());
+		userDetails.setDescription("Description of user 1");
 		
 		SessionFactory sessionFactory = new Configuration().configure().buildSessionFactory();
 		
--- JavaBrainsHibernate/src/main/java/com/javabrains/hibernate/entity/UserDetails.java
+++ JavaBrainsHibernate/src/main/java/com/javabrains/hibernate/entity/UserDetails.java
@@ -1,26 +1,43 @@
 package com.javabrains.hibernate.entity;
 
-import javax.persistence.Column;
+import java.util.Date;
+
+//import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
 
-@Entity (name="USER_DETAILS")   // not necessary, but a good way to specific if not defaulting it to classname
+@Entity //(name="USER_DETAILS")   // not necessary, but a good way to specific if not defaulting it to classname
+@Table (name="USER_DETAILS")      // keeps entity UserDetails but maps to a specific table
 public class UserDetails {
 	
 	@Id
-	@Column (name="USER_ID")
+	//@Column (name="USER_ID")  // @Column allows you to map a column to a property that doesn't match name wise
 	private int userId;
-	@Column (name= "USER_NAME", nullable=false)
+	//@Column (name= "USER_NAME", nullable=false)
+	//@Basic (optional=false)
+	//@Transient   // makes it skip this field from a DB standpoint (column won't exist)
 	private String userName;
+	@Temporal (TemporalType.DATE)  // allows you to specify whether it's a date, timestamp without tz (default), etc... 
+	private Date joinedDate;
+	private String address;
+	@Lob // hibernate will determine whether BLOB or CLOB based on the java type (CLOB in this case)
+	private String description;
 	
 	public UserDetails() {
 		
 	}
 	
-	public UserDetails(int userId, String userName) {
+	public UserDetails(int userId, String userName, Date joinedDate, String address, String description) {
 		super();
 		this.userId = userId;
 		this.userName = userName;
+		this.joinedDate = joinedDate;
+		this.address = address;
+		this.description = description;
 	}
 
 	public int getUserId() {
@@ -35,7 +52,22 @@ public class UserDetails {
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
-	
-	
-
+	public Date getJoinedDate() {
+		return joinedDate;
+	}
+	public void setJoinedDate(Date joinedDate) {
+		this.joinedDate = joinedDate;
+	}
+	public String getAddress() {
+		return address;
+	}
+	public void setAddress(String address) {
+		this.address = address;
+	}
+	public String getDescription() {
+		return description;
+	}
+	public void setDescription(String description) {
+		this.description = description;
+	}
 }
