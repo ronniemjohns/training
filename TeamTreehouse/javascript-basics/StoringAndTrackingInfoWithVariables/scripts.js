3916905ef250827e95c970d4a7bb28db869c56aa
Ronnie Johns <Ronnie.Johns@USAA.com>
2017-04-25 16:57:48 -0500

    Ronnie - testing variables

--- TeamTreehouse/javascript-basics/StoringAndTrackingInfoWithVariables/scripts.js
+++ TeamTreehouse/javascript-basics/StoringAndTrackingInfoWithVariables/scripts.js
@@ -1,3 +1,16 @@
-/**
- * Created by Frank on 4/24/2017.
- */
+var message = "Welcome to treehouse";
+var anotherMessage = 'Welcome to my house';
+
+alert(message);
+alert(anotherMessage);
+
+// notice putting a single quote inside a string
+message = "She's a great person";
+
+// or ditto for double quotes:
+message = '<h1 class="special">Important Headline</h1>';
+alert(message);
+
+
+// although you can escape with the backslash:
+message = 'She\'s an important person';
