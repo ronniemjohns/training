a331e69f285d2f5fd02beea5ab9b524cc76b46f8
Ronnie Johns <Ronnie.Johns@USAA.com>
2017-05-10 17:48:09 -0500

    Ronnie - playing with arrays.

--- TeamTreehouse/JavaScript-Loops-Arrays-Objects/JavaScript_Loops_Arrays_Objects/Section_2_Arrays/video05/js/playlist.js
+++ TeamTreehouse/JavaScript-Loops-Arrays-Objects/JavaScript_Loops_Arrays_Objects/Section_2_Arrays/video05/js/playlist.js
@@ -11,3 +11,13 @@ function print(message) {
   document.write(message);
 }
 
+function printList( list ) {
+  var listHTML = '<ol>';
+  for (var i=0; i< list.length; i++ ) {
+    listHTML += "<li>" + list[i] + "</li>";
+  }
+  listHTML += "</ol>";
+  print(listHTML);
+}
+
+printList(playList);
--- TeamTreehouse/JavaScript-Loops-Arrays-Objects/JavaScript_Loops_Arrays_Objects/Section_2_Arrays/video06/js/search.js
+++ TeamTreehouse/JavaScript-Loops-Arrays-Objects/JavaScript_Loops_Arrays_Objects/Section_2_Arrays/video06/js/search.js
@@ -1,6 +1,9 @@
 var inStock = [ 'apples', 'eggs', 'milk', 'cookies', 'cheese', 'bread', 'lettuce', 'carrot', 'broccoli', 'pizza', 'potato', 'crackers', 'onion', 'tofu', 'frozen dinner', 'cucumber'];
 var search;
 
+for(var i = 0; i < inStock.length; i++) {
+  print(inStock[i]);
+}
 function print(message) {
   document.write( '<p>' + message + '</p>');
 }
