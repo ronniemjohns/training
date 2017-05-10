f3afd1c853b0b63dbb3b9c7dc2706ebbee461d28
Ronnie Johns <Ronnie.Johns@USAA.com>
2017-05-10 13:38:21 -0500

    Ronnie - finished section 1.

--- TeamTreehouse/JavaScript-Loops-Arrays-Objects/JavaScript_Loops_Arrays_Objects/Section_1_ Loops/video03/app.js
+++ TeamTreehouse/JavaScript-Loops-Arrays-Objects/JavaScript_Loops_Arrays_Objects/Section_1_ Loops/video03/app.js
@@ -1,3 +1,19 @@
-function randomNumber(upper) {
+var upper = 1000000;
+var randomNumber = getRandomNumber(upper);
+var guess = 0;
+var attempts = 0;
+
+
+function getRandomNumber(upper) {
   return Math.floor( Math.random() * upper ) + 1;
 }
+
+
+while (guess !== randomNumber) {
+    guess = getRandomNumber(upper);
+    attempts++;
+}
+
+document.write("<p> RandomNumber: " + randomNumber + "<p>");
+document.write("<p> It took " + attempts + " attempts to guess it. <p>");
+
---
+++ TeamTreehouse/JavaScript-Loops-Arrays-Objects/JavaScript_Loops_Arrays_Objects/Section_1_ Loops/video05/js/script.js
@@ -0,0 +1,10 @@
+/**
+ * Created by plr3269 on 5/10/2017.
+ */
+
+var html = "";
+
+for(var counter = 0; counter < 10; counter++) {
+    html += "<div>" + counter + "</div>";
+}
+document.write(html);
