119312e8bb4d867be17a0620ac76d7b656c156dc
Ronnie Johns <rjohns7@gatech.edu>
2017-05-12 22:55:55 -0500

    Ronnie - complete intro to methods.

--- TeamTreehouse/JavaScript-Object-Oriented/1_Dice/2_dice_understanding_this_start/dice.js
+++ TeamTreehouse/JavaScript-Object-Oriented/1_Dice/2_dice_understanding_this_start/dice.js
@@ -1,7 +1,7 @@
 var dice = {
+  sides : 6,
   roll: function () {
-    var sides = 6;
-    var randomNumber = Math.floor(Math.random() * sides) + 1;
+    var randomNumber = Math.floor(Math.random() * this.sides) + 1;
     console.log(randomNumber);
   }
 }
--- TeamTreehouse/JavaScript-Object-Oriented/1_Dice/3_dice_returnting_values_start/dice.js
+++ TeamTreehouse/JavaScript-Object-Oriented/1_Dice/3_dice_returnting_values_start/dice.js
@@ -2,7 +2,7 @@ var dice = {
   sides: 6,
   roll: function () {
     var randomNumber = Math.floor(Math.random() * this.sides) + 1;
-    console.log(randomNumber);
+      return randomNumber;
   }
 }
 
