429c2a72c75144338976d582d2ce1e3cd81ccc91
Ronnie Johns <Ronnie.Johns@USAA.com>
2017-05-24 10:25:43 -0500

    Ronnie - added React Router Basics course.

---
+++ TeamTreehouse/React-Router-Basics/1-getting-started-with-react-router/video-5/course-directory_final/src/index.js
@@ -0,0 +1,34 @@
+// Libs
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, Route, hashHistory } from 'react-router';
+
+// CSS 
+import './css/style.css';
+
+// Components
+import App from './components/App';
+import Home from './components/Home';
+import About from './components/About';
+import Courses from './components/Courses';
+import Teachers from './components/Teachers';
+
+// Render
+render((
+  <Router history={hashHistory}>
+    <Route path="/" component={Home} />
+    <Route path="about" component={About} />
+    <Route path="teachers" component={Teachers} />
+    <Route path="courses" component={Courses} />
+  </Router>
+), document.getElementById('root'));
+
+
+
+
+
+
+
+
+
+
