0982392b1cd94d28389cfb3fd9a602d2322c10a3
Ronnie Johns <rjohns7@gatech.edu>
2017-05-30 00:00:05 -0500

    Ronnie - completed section 2.

--- TeamTreehouse/Building-Applications-with-React-and-Redux/2-modularizing-the-stopwatch-app/2-stopwatch-logical-component/react-redux-course_final/src/containers/Scoreboard.js
+++ TeamTreehouse/Building-Applications-with-React-and-Redux/2-modularizing-the-stopwatch-app/2-stopwatch-logical-component/react-redux-course_final/src/containers/Scoreboard.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import Stopwatch from '../components/Stopwatch';
+
 const INITIAL_STATE = {
   players: [
     {
@@ -15,7 +18,7 @@ const INITIAL_STATE = {
   ],
 }
 
-const Application = React.createClass({
+const Scoreboard = React.createClass({
   getInitialState: function () {
     return INITIAL_STATE;
   },
@@ -101,73 +104,6 @@ Stats.propTypes = {
   players: React.PropTypes.array.isRequired,
 };
 
-// Move to components/Stopwatch.js
-// ------------------------------------------------------------------------
-const Stopwatch = React.createClass({
-  getInitialState: function () {
-    return ({
-      running: false,
-      previouseTime: 0,
-      elapsedTime: 0,
-    });
-  },
-
-  componentDidMount: function () {
-    this.interval = setInterval(this.onTick);
-  },
-
-  componentWillUnmount: function () {
-    clearInterval(this.interval);
-  },
-
-
-  onStart: function () {
-    this.setState({
-      running: true,
-      previousTime: Date.now(),
-    });
-  },
-
-  onStop: function () {
-    this.setState({
-      running: false,
-    });
-  },
-
-  onReset: function () {
-    this.setState({
-      elapsedTime: 0,
-      previousTime: Date.now(),
-    });
-  },
-
-  onTick: function () {
-    if (this.state.running) {
-      var now = Date.now();
-      this.setState({
-        elapsedTime: this.state.elapsedTime + (now - this.state.previousTime),
-        previousTime: Date.now(),
-      });
-    }
-  },
-
-  render: function () {
-    var seconds = Math.floor(this.state.elapsedTime / 1000);
-    return (
-      <div className="stopwatch" >
-        <h2>Stopwatch</h2>
-        <div className="stopwatch-time"> {seconds} </div>
-        { this.state.running ?
-          <button onClick={this.onStop}>Stop</button>
-          :
-          <button onClick={this.onStart}>Start</button>
-        }
-        <button onClick={this.onReset}>Reset</button>
-      </div>
-    )
-  }
-});
-
 // Move to components/Player.js
 // ----------------------------------------------------------------------
 function Player(props) {
@@ -249,3 +185,4 @@ const AddPlayerForm = React.createClass({
   }
 });
 
+export default Scoreboard;
---
+++ TeamTreehouse/Building-Applications-with-React-and-Redux/2-modularizing-the-stopwatch-app/5-player-and-header-pure-components/react-redux-course_final/src/containers/Scoreboard.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import Player from '../components/Player';
+import Header from '../components/Header';
+import AddPlayerForm from '../components/AddPlayerForm';
+
+const INITIAL_STATE = {
+  players: [
+    {
+      name: 'Jim Hoskins',
+      score: 31,
+    },
+    {
+      name: 'Andrew Chalkley',
+      score: 20,
+    },
+    {
+      name: 'Alena Holligan',
+      score: 50,
+    },
+  ],
+}
+
+const Scoreboard = React.createClass({
+  getInitialState: function () {
+    return INITIAL_STATE;
+  },
+  onScoreChange: function(index, delta) {
+    this.state.players[index].score += delta;
+    this.setState(this.state);
+  },
+
+  onAddPlayer: function(name) {
+    this.state.players.push({ name: name, score: 0 });
+    this.setState(this.state);
+  },
+
+  onRemovePlayer: function(index) {
+    this.state.players.splice(index, 1);
+    this.setState(this.state);
+  },
+
+  render: function() {
+    return (
+      <div className="scoreboard">
+        <Header players={this.state.players} />
+        <div className="players">
+          {this.state.players.map(function(player, index) {
+             return (
+               <Player
+                 name={player.name}
+                 score={player.score}
+                 key={player.name}
+                 onScoreChange={(delta) => this.onScoreChange(index, delta)}
+                 onRemove={() => this.onRemovePlayer(index)}
+               />
+             );
+           }.bind(this))}
+        </div>
+        <AddPlayerForm onAdd={this.onAddPlayer} />
+      </div>
+    );
+  }
+});
+
+export default Scoreboard;
